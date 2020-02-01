3d860c35d0255df65a15457d08698f9c2c993a83
AnimationCube <nguyentutuyen45@gmail.com>
2020-02-01 21:49:50 +0700

    interface

--- Step_02/func.js
+++ Step_02/func.js
@@ -4,3 +4,12 @@ function info(number1, number2) {
     return number1 + number2;
 }
 console.log(info(4, 5));
+;
+function fullName(person /*{firstName:string, lastName:string}*/) {
+    console.log(person.firstName + " " + person.lastName);
+}
+var p = {
+    firstName: 'Nguyễn Tự',
+    lastName: 'Tuyên'
+};
+fullName(p);
--- Step_02/func.ts
+++ Step_02/func.ts
@@ -3,3 +3,23 @@ function info(number1:number, number2:number) {
     return number1+number2;
 }
 console.log(info(4,5));
+
+
+//interface
+interface Person {
+    firstName:string;
+    lastName : string;
+};
+
+
+function fullName(person: Person/*{firstName:string, lastName:string}*/) {
+    console.log(person.firstName +" "+ person.lastName)
+    
+}
+
+let p = {
+    firstName:'Nguyễn Tự',
+    lastName:'Tuyên'
+}
+fullName(p);
+
